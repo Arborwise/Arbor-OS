80af16b78282657b28f95b8ec36fd91e7102492e
Arborwise <greg@arborwisetreecare.com>
2026-08-02 17:07:06 -0500

    Load Annie logo launch and branchless flight update

--- website-preview/professional.js
+++ website-preview/professional.js
@@ -3,8 +3,8 @@
 
   const BASE_BUILD='https://cdn.jsdelivr.net/gh/Arborwise/Arbor-OS@f63013983ef3a82fe01acb614a710122e5c57a81/website-preview/professional.js';
   const ENHANCEMENTS=[
-    'chamber-memberships-v16.js?v=20260802-1714',
-    'annie-scroll-flight-v16.js?v=20260802-1714'
+    'chamber-memberships-v16.js?v=20260802-1610',
+    'annie-scroll-flight-v16.js?v=20260802-1610'
   ];
 
   function installReadOnlyAnnie(){
