a2f613191cacfd9abe15dc372077b62c534da0fb
Arborwise <greg@arborwisetreecare.com>
2026-08-03 10:50:16 -0500

    Load orange CTA, rebuilt bubble, and corrected trademark

--- website-preview/hero-unify-v48.js
+++ website-preview/hero-unify-v48.js
@@ -5,9 +5,11 @@
   if (oldV49) oldV49.remove();
   const oldV50 = document.querySelector('script[data-aw-hero-v50]');
   if (oldV50) oldV50.remove();
+  const oldV51 = document.querySelector('script[data-aw-hero-v51]');
+  if (oldV51) oldV51.remove();
   const script = document.createElement('script');
-  script.src = 'hero-unify-v50.js?rev=spring-green-bubble-20260803-1042';
+  script.src = 'hero-unify-v51.js?rev=orange-bubble-tm-20260803-1048';
   script.async = false;
-  script.dataset.awHeroV50 = 'true';
+  script.dataset.awHeroV51 = 'true';
   document.head.appendChild(script);
 })();
