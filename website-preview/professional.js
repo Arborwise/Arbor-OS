a99f2c025a862a25ec125984b759857ce2de7a28
Arborwise <greg@arborwisetreecare.com>
2026-08-02 21:09:52 -0500

    Load corrected Annie circle and compact landing sequence

--- website-preview/professional.js
+++ website-preview/professional.js
@@ -28,7 +28,7 @@
   const BASE_BUILD='https://cdn.jsdelivr.net/gh/Arborwise/Arbor-OS@f63013983ef3a82fe01acb614a710122e5c57a81/website-preview/professional.js';
   const ENHANCEMENTS=[
     'chamber-memberships-v16.js?v=20260802-1610',
-    'annie-character-v28.js?v=20260802-2110'
+    'annie-character-v29.js?v=20260802-2122'
   ];
 
   function installReadOnlyAnnie(){
